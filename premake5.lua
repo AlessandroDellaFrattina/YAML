4e6344f4c15f87468b2962865b9669a6798b8864
asmdenc1 <alessandrodellafrattina@gmail.com>
2024-01-07 15:48:27 +0100

    Changed staticruntime from "On" to "Off"

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ project "YAML"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"
-	staticruntime "On"
+	staticruntime "Off"
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
