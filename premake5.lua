4436889393b8e73f001e05a54b9a9dc1afa99694
asmdenc1 <alessandrodellafrattina@gmail.com>
2024-04-05 18:44:10 +0200

    Updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -3,8 +3,8 @@ project "YAML"
 	language "C++"
 	cppdialect "C++17"
 	staticruntime "Off"
-	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/lib")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/lib")
 
 	includedirs {
 
